0c83dcef525d6c8fb0768cd35a03f0bd58c41050
kanwar911 <156605689+kanwar911@users.noreply.github.com>
2025-01-29 16:14:46 -0500

    Update q1.py

--- act btp/q1.py
+++ act btp/q1.py
@@ -1,1 +1,13 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, num): 
+        if num % i == 0:
+            return False
+    return True
+
 def getPrimeNumbers(n):
+    return [x for x in range(2, n + 1) if is_prime(x)]
+
+n = int(input("Enter a number: "))
+print(getPrimeNumbers(n))
